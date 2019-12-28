7e8ce458aebc175a7ae082f1e177d3fc7b20e269
skywalkaa <49798726+skywalkaa@users.noreply.github.com>
2019-12-28 12:51:38 -0700

    Update Player.py

--- Player.py
+++ Player.py
@@ -40,22 +40,18 @@ class Player:
                 total[0] += 1
                 total[1] += 11
             elif i.idNum % 13 in [0,11,12]:
-                total[0] += 11
-                total[1] += 11
+                total[0] += 10
+                total[1] += 10
             else:
                 total[0] += i.idNum % 13
                 total[1] += i.idNum % 13
 
         return total
 
-class NPCPlayer(Player):
+class Dealer(Player):
 
-    def __init__(self, number, name, money, risk=None):
-        self.number = number
-        self.name   = name
-        self.money  = money
+    def __init__(self, risk=None):
         self.hand   = []
-
         if risk is None:
             self.risk = gauss(0.5,0.3);
         else
@@ -70,4 +66,3 @@ class NPCPlayer(Player):
             if handValue < 11:
                 return True
             elif handValue < 13+2*risk+(2*(random()-.5)):
-                if random()*self.risk 
